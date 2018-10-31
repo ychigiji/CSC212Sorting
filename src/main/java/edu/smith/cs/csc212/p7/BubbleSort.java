c7b5b4e86c8779ac5493884d57bf50369a23aa38
John Foley <jfoley@cs.umass.edu>
2018-10-31 16:57:27 -0400

    Bubble Sort!

--- src/main/java/edu/smith/cs/csc212/p7/BubbleSort.java
+++ src/main/java/edu/smith/cs/csc212/p7/BubbleSort.java
@@ -2,17 +2,21 @@ package edu.smith.cs.csc212.p7;
 
 import java.util.List;
 
+/**
+ * BubbleSort is a simple sort that is O(n^2).
+ * @author jfoley
+ *
+ */
 public class BubbleSort {
-	public static boolean isSorted(List<Integer> items) {
-		for (int i=0; i<items.size()-1; i++) {
-			if (items.get(i) >= items.get(i+1)) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
+
+	/**
+	 * Swap items in a list!
+	 * @param items the list.
+	 * @param i swap the item at this index with the one at j.
+	 * @param j swap the item at this index with the one at i.
+	 */
 	public static void swap(List<Integer> items, int i, int j) {
+		assert(i != j);
 		int tmp = items.get(i);
 		items.set(i,  items.get(j));
 		items.set(j, tmp);
@@ -20,17 +24,15 @@ public class BubbleSort {
 	public static void bubbleSort(List<Integer> input) {
 		int N = input.size();
 		
-		System.out.println("Sorting: "+input);
-
 		while(true) {
 			boolean sorted = true;
 			for (int i=0; i<N-1; i++) {
-				if (input.get(i) >= input.get(i+1)) {
+				if (input.get(i) > input.get(i+1)) {
 					swap(input, i, i+1);
 					sorted = false;
 				}
 			}
-			if (!sorted) {
+			if (sorted) {
 				break;
 			}
 			N = N - 1;
--- src/test/java/edu/smith/cs/csc212/p7/TestSorting.java
+++ src/test/java/edu/smith/cs/csc212/p7/TestSorting.java
@@ -1,6 +1,12 @@
 package edu.smith.cs.csc212.p7;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestSorting {
 	/**
@@ -8,18 +14,34 @@ public class TestSorting {
 	 * @param items - the list of integers.
 	 * @return true if it is sorted, false if not.
 	 */
-	private static boolean isSorted(List<Integer> items) {
+	private static boolean checkSorted(List<Integer> items) {
 		for (int i=0; i<items.size()-1; i++) {
 			if (items.get(i) >= items.get(i+1)) {
+				System.err.println("items out of order: "+items.get(i)+", "+items.get(i+1) + " at index="+i);
 				return false;
 			}
 		}
 		return true;
 	}
 	
+	/**
+	 * Here's some data!
+	 */
+	private static int[] data = {9,8,7,6,5,4,3,2,1};
+	
 	@Test
 	public void testBubbleSort() {
+		// See if the data can be reversed:
+		ArrayList<Integer> sortMe = new ArrayList<>();
+		for (int y : data) {
+			sortMe.add(y);
+		}
+		BubbleSort.bubbleSort(sortMe);
+		Assert.assertTrue(checkSorted(sortMe));
 		
-	}
-	
+		// For good measure, let's shuffle it and sort it again to see if that works, too.
+		Collections.shuffle(sortMe);
+		BubbleSort.bubbleSort(sortMe);
+		Assert.assertTrue(checkSorted(sortMe));
+	}	
 }
